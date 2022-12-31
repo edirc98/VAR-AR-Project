e13bee2dede885c8bb577f1ed02662ba5a2a0383
Edgar <edirc98@gmail.com>
2022-12-31 17:47:46 +0100

    Changed markers instanciation, not working very well

---
+++ Assets/Scripts/HenMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HenMovement : MonoBehaviour
+{
+    public bool isMoving = false; 
+    public float radius;
+    public float minDistanceToPos;
+    public float movementSpeed;
+    public float rotationSpeed; 
+    private Vector3 newPos;
+    private Vector3 currentPos;
+    private Vector3 looktoTarget; 
+
+    private float pos = 0; 
+
+    private bool findNewPos = true;
+    private bool movingToPos = false; 
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            if (findNewPos)
+            {
+                currentPos = transform.position;
+                newPos = new Vector3(currentPos.x + Random.Range(-radius, radius), 0, currentPos.z + Random.Range(-radius, radius));
+                looktoTarget = (newPos - currentPos).normalized;
+                findNewPos = false;
+                movingToPos = true;
+            }
+            if (movingToPos)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(looktoTarget), Time.deltaTime * rotationSpeed);
+
+                transform.position = Vector3.MoveTowards(transform.position, newPos, Time.deltaTime * movementSpeed);
+                if (Vector3.Distance(transform.position, newPos) < minDistanceToPos)
+                {
+                    movingToPos = false;
+                    pos = 0;
+                    findNewPos = true;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+        Gizmos.DrawLine(currentPos, newPos);
+
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawSphere(newPos, 0.1f);
+        
+        
+    }
+}
--- Assets/Scripts/PlaceTrackedImages.cs
+++ Assets/Scripts/PlaceTrackedImages.cs
@@ -11,14 +11,21 @@ public class PlaceTrackedImages : MonoBehaviour
     private ARTrackedImageManager _trackedImagesManager;
 
     // List of prefabs to instantiate as their corresponding 2D images
-    public GameObject[] ArPrefabs;
+    public GameObject[] ArPrefabsToPlace;
 
     // Keep dictionary array of created prefabs
-    private readonly Dictionary<string, GameObject> _instatiatedPrefabs = new Dictionary<string, GameObject>();
+    private readonly Dictionary<string, GameObject> _ArPrefabs = new Dictionary<string, GameObject>();
 
     void Awake()
     {
         _trackedImagesManager = GetComponent<ARTrackedImageManager>();
+
+        foreach(GameObject arPrefab in ArPrefabsToPlace)
+        {
+            GameObject newArObject = Instantiate(arPrefab, Vector3.zero, Quaternion.identity);
+            newArObject.name = arPrefab.name;
+            _ArPrefabs.Add(arPrefab.name, newArObject);
+        }
     }
 
     void OnEnable()
@@ -34,28 +41,53 @@ public class PlaceTrackedImages : MonoBehaviour
     // Event Handler
     private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-        foreach(var trackedImage in eventArgs.added)
+       
+        foreach (var trackedImage in eventArgs.added)
         {
-            var imageName = trackedImage.referenceImage.name;
-            foreach(var curPrefab in ArPrefabs)
+            UpdateARImage(trackedImage);
+            //var imageName = trackedImage.referenceImage.name;
+            //foreach (var curPrefab in ArPrefabsToPlace)
+            //{
+            //    if (string.Compare(curPrefab.name, imageName, StringComparison.OrdinalIgnoreCase) == 0 && !_ArPrefabs.ContainsKey(imageName))
+            //    {
+            //        var newPrefab = Instantiate(curPrefab, trackedImage.transform);
+            //        _ArPrefabs[imageName] = newPrefab;
+            //    }
+            //}
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdateARImage(trackedImage); 
+        //    _instatiatedPrefabs[trackedImage.referenceImage.name].SetActive(trackedImage.trackingState == TrackingState.Tracking);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        {
+            _ArPrefabs[trackedImage.name].SetActive(false);
+        //    Destroy(_instatiatedPrefabs[trackedImage.referenceImage.name]);
+        //    _instatiatedPrefabs.Remove(trackedImage.referenceImage.name);
+        }
+    }
+
+    private void UpdateARImage(ARTrackedImage trackedImage)
+    {
+        AssignGameObject(trackedImage.referenceImage.name, trackedImage.transform.position); 
+    }
+
+    private void AssignGameObject(string name, Vector3 position)
+    {
+        if(ArPrefabsToPlace != null)
+        {
+            _ArPrefabs[name].SetActive(true);
+            _ArPrefabs[name].transform.position = position;
+            foreach(GameObject go in _ArPrefabs.Values)
             {
-                if (string.Compare(curPrefab.name, imageName, StringComparison.OrdinalIgnoreCase) == 0 && !_instatiatedPrefabs.ContainsKey(imageName))
+                if(go.name != name)
                 {
-                    var newPrefab = Instantiate(curPrefab, trackedImage.transform);
-                    _instatiatedPrefabs[imageName] = newPrefab;
+                    go.SetActive(false);
                 }
             }
-        }
-
-        foreach (var trackedImage in eventArgs.updated)
-        {
-            _instatiatedPrefabs[trackedImage.referenceImage.name].SetActive(trackedImage.trackingState == TrackingState.Tracking);
-        }
-
-        foreach (var trackedImage in eventArgs.removed)
-        {
-            Destroy(_instatiatedPrefabs[trackedImage.referenceImage.name]);
-            _instatiatedPrefabs.Remove(trackedImage.referenceImage.name);
         }
     }
 }
